dccd68275c2adeb4685595dd784efbc3f49eac88
PATIENCE KANDARE <PATIENCE.KANDARE@HOTMAIL.COM>
2026-02-15 21:51:40 +0000

    create thumbnail bar with active feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,11 @@ import './App.css'
 export default function App() {
 
   const [photos, setPhotos] = useState([])
+  const [currentIndex, setCurrentIndex] = useState(0)
   
   async function fetchCollection() {
     const res = await fetch("http://localhost:3000/api/unsplash/collection?id=qeQURMsKYh0");
     const data = await res.json();
-
-    console.log("Full response data:", data)
-    console.log("Number of photos received:", data.length)
 
     setPhotos(data)
   }
@@ -20,20 +18,36 @@ export default function App() {
     fetchCollection();
   }, [])
 
+  function handleClick(index) {
+    setCurrentIndex(index)
+  }
+
   return (
-    <div> 
+    <div className="photos"> 
+    {photos.length > 0 && (
+      <div className='background-image'
+      style={{
+        backgroundImage:`url(${photos[currentIndex].urls.regular})`,
+        backgroundSize: "cover"
+      }}
+     /> 
+    )}
+
+    <div className="thumbnails"> 
     {photos.map ((photo, index) => (
-      <div key={photo.id}>
-        {photo.urls?.small && (
-          <img key={photo.id || index} 
+      <img key={photo.id}
         src={photo.urls.small} 
         alt={photo.alt_description || "Holiday destination"}
+        onClick={() => handleClick(index)}
+        className={index === currentIndex ? "active" : ""}
         />
-        )}
+        ))}
       </div>
-    ))}
-    </div> 
-  )
-}
+      </div>
+    );
+  }
+ 
+
+
 
 
